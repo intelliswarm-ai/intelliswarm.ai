9289844a6c9ffc1b7c76fb62b3a3b2a569fe9b7f
Theodoros Messinis <thodoris.messinis@gmail.com>
2025-02-23 17:58:57 +0100

    First Bits

--- intelliswarm-angular/src/app/app-routing.module.ts
+++ intelliswarm-angular/src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { CaseStudiesComponent } from './case-studies/case-studies.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'case-studies', component: CaseStudiesComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
--- intelliswarm-angular/src/app/app.module.ts
+++ intelliswarm-angular/src/app/app.module.ts
@@ -2,22 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+// import { AboutComponent } from './about/about.component';
 
 // Import the newly created components
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ServicesComponent } from './services/services.component';
 import { CaseStudiesComponent } from './case-studies/case-studies.component';
 import { ContactComponent } from './contact/contact.component';
 
+import { HeroComponent } from './hero/hero.component';
+import { FeaturesComponent } from './features/features.component';
+import { ProductsComponent } from './products/products.component';
+
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
     HomeComponent,
-    AboutComponent,
+    // AboutComponent
     ServicesComponent,
     CaseStudiesComponent,
-    ContactComponent
+    ContactComponent,
+    HeroComponent,
+    FeaturesComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
